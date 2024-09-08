44bbd31fdb5f322af0ac32b050f3cf3f2553db5e
Algosphere <93322746+AlgoSphere-Dev@users.noreply.github.com>
2024-09-08 18:55:57 +0900

    Delete main directory

--- main/js/main.js
+++
@@ -1,7 +0,0 @@
-var randomNumber = 0;
-function change() {
-	randomNumber = Math.floor(Math.random() * 1000) + 1;
-	loc.style.top = randomNumber;
-	randomNumber = Math.floor(Math.random() * 1000) + 1;
-	loc.style.left = randomNumber;
-}
